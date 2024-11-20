7bddaf81e24c4bd1be2ab98a7f0d9029227517c8
yuyi <yuyi.wsy@oceanbase.com>
2024-11-20 19:02:15 +0800

    fix(misc): escape @ in database password

--- connection.py
+++ connection.py
@@ -7,6 +7,6 @@ connection_args = {
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
     "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
+    "password": os.getenv("DB_PASSWORD").replace("@", "%40") if os.getenv("DB_PASSWORD") else None,
     "db_name": os.getenv("DB_NAME"),
 }
